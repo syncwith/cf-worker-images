e0b9b621b22b67708e9ba09105231573703ab431
Alex Black <alex@alexblack.ca>
2023-05-11 16:14:30 -0700

    remove log

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,6 @@ export default {
     console.log(origRequest.url);
     const request = getActualRequest(env, origRequest);
 
-    console.log(request.headers.get("User-Agent"));
-
     // If its a request for an image, and its NOT a request from Cloudinary for a source iamge
     // Then we want to handle it and return an optimized images from Cloundary
     if (isImageRequest(request) && !isCloudinaryRequest(request)) {
